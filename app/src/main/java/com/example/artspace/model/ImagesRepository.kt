451194432a7d4fb5321c01d4cb045b8a3dcf0f42
Alexandra <sashaf708@gmail.com>
2025-03-03 00:27:38 +0400

    feat: перенос описания

--- app/src/main/java/com/example/artspace/model/ImagesRepository.kt
+++ app/src/main/java/com/example/artspace/model/ImagesRepository.kt
@@ -4,11 +4,11 @@ import com.example.artspace.R
 
 object ImagesRepository{
     val images = listOf(
-    Image(R.drawable.image1, R.string.description1),
-    Image(R.drawable.image2, R.string.description2),
-    Image(R.drawable.image3, R.string.description3),
-    Image(R.drawable.image4, R.string.description4),
-    Image(R.drawable.image5, R.string.description5)
+    Image(R.drawable.image1),
+    Image(R.drawable.image2),
+    Image(R.drawable.image3),
+    Image(R.drawable.image4),
+    Image(R.drawable.image5)
     )
     fun GetImagesSize(): Int{
         return images.size
